6d4e2dfd71e7af36d9cc0bc435aa9cd01c5a2870
Lukman Balunywa <Lukman@taacs.cloud>
2022-09-16 14:51:04 -0400

    updtesd

---
+++ config.py
@@ -0,0 +1,6 @@
+ACCOUNT_NAME = "nsgflowlogss"
+ACCOUNT_KEY = "8JE2zMRrCL0V3pI535YVGMLLfjcd9aj+eumgom/Dy2T/fQm3qW9dVxDHqmDMSMcGxNslpzkNV7wY+ASt4m16iA=="
+CONNECTION_STRING = "https://nsgflowlogss.blob.core.windows.net"
+CONTAINER = "zureportaldeploy"
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg'])
+MAX_CONTENT_LENGTH = 20 * 1024 * 1024    # 20 Mb limit
